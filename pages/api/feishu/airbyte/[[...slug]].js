4ec44a8dffe83573c1cefd8bdee324e3547059b5
kaokei <kaokei@gmail.com>
2023-04-18 21:15:31 +0800

    feat: 增加airbyte的飞书的webhook

---
+++ pages/api/feishu/airbyte/[[...slug]].js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  try {
+    await axios.post(req.query.url, getData(req));
+    res.send("webhook forward success");
+  } catch (e) {
+    console.log("dingding sentry axios post error :>> ", e);
+    res.send("webhook forward failed");
+  }
+}
+
+function getData(req) {
+  var data = req.body || {};
+  var keyword = req.query.keyword || "监控报警";
+
+  const str = JSON.stringify(data);
+
+  const res = { msg_type: "text", content: { text: keyword + ": " + str } };
+
+  return res;
+}
