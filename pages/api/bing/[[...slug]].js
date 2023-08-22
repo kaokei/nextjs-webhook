f4adf7ed042ecb239310890d019c8024450278d7
kaokei <kaokei.rst@gmail.com>
2023-08-22 16:38:06 +0800

    修改res为result

--- pages/api/bing/[[...slug]].js
+++ pages/api/bing/[[...slug]].js
@@ -3,12 +3,12 @@ import axios from "axios";
 export default async function handler(req, res) {
   try {
     const image = req.query.image || "";
-    const res = await axios.get('https://cn.bing.com/hp/api/model')
+    const result = await axios.get('https://cn.bing.com/hp/api/model')
     if (image) {
-      const item0 = res.data.MediaContents[0];
+      const item0 = result.data.MediaContents[0];
       res.send(item0.ImageContent.Image.Url)
     } else {
-      res.send(res.data)
+      res.send(result.data)
     }
   } catch (e) {
     console.log("Get bing image error :>> ", e);
