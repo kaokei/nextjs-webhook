ed3def2ae0cb3859a0952cb1535a34363b951d53
kaokei <kaokei.rst@gmail.com>
2023-08-22 16:55:39 +0800

    使用302重定向

--- pages/api/bing/[[...slug]].js
+++ pages/api/bing/[[...slug]].js
@@ -3,12 +3,15 @@ import axios from "axios";
 export default async function handler(req, res) {
   try {
     const image = req.query.image || "";
-    const result = await axios.get('https://cn.bing.com/hp/api/model')
+    const result = await axios.get('https://cn.bing.com/hp/api/model');
     if (image) {
-      const item0 = result.data.MediaContents[0];
-      res.send(item0.ImageContent.Image.Url)
+      let url = result.data.MediaContents[0];
+      if(url.indexOf('http') !== 0) {
+        url = 'https://cn.bing.com' + url;
+      }
+      res.redirect(302, url);
     } else {
-      res.send(result.data)
+      res.send(result.data);
     }
   } catch (e) {
     console.log("Get bing image error :>> ", e);
